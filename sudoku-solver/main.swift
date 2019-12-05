672e91543be1428f0cc18319b9de308991367250
Sebastian Bauer <sebastianbauer96@gmail.com>
2019-12-05 14:32:30 +0100

    Initial Commit

---
+++ sudoku-solver/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sudoku-solver
+//
+//  Created by Sebastian Bauer on 05.12.19.
+//  Copyright © 2019 Sebastian Bauer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
